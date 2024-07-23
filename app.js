d7e2636f86bd2d250e5f5bea47cdaa431ea30ad2
Amanvir <amanvir.singh880@gmail.com>
2024-07-22 21:07:45 -0400

    Authentication with Google Implemented

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketIo = require("socket.io");
+const authRouter = require('./public/routes/auth');
+const session = require('express-session');
+const SQLiteStore = require('connect-sqlite3')(session);
+const passport = require('passport');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,15 +17,49 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const middleware = session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+});
+
+
+app.use(middleware);
+app.use(passport.authenticate('session'));
+
+app.use('/', authRouter);
+
+io.use((socket, next) => {
+  middleware(socket.request, {}, next);
+});
+
+
+io.use((socket, next) => {
+  passport.initialize()(socket.request, {}, () => {
+    passport.session()(socket.request, {}, next);
+  });
+});
+
+
+io.use((socket, next) => {
+  if (socket.request.user) {
+    next();
+  } else {
+    next(new Error('Unauthorized'));
+  }
+});
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { user: req.user });
 });
 
 let checkboxStates = Array(100).fill(false);
 
 io.on("connection", (socket) => {
-  console.log("New client connected");
+  const user = socket.request.user;
+  console.log(`New client connected`);
 
   socket.emit("initialState", checkboxStates);
 
@@ -32,7 +70,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
+    console.log(`Client disconnected`);
   });
 });
 
